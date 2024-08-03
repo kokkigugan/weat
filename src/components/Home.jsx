c137c1e43632adc1468d5ee1ca7132815a472a73
Gugan <110531967+kokkigugan@users.noreply.github.com>
2024-08-03 19:27:03 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,6 @@ import Forecast from "./Forecast";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function Home() {
-  // State to manage the search query and weather data
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({
     loading: true,
@@ -13,28 +12,23 @@ function Home() {
     error: false,
   });
 
-  // Function to handle the search when the Enter key is pressed
   const search = async (event) => {
     if (event.key === "Enter") {
       event.preventDefault();
-      setQuery(""); // Clear the query input
-      setWeather({ ...weather, loading: true }); // Set loading state
-
-      // API key and URL for weather data
+      setQuery(""); 
+      setWeather({ ...weather, loading: true }); 
       const apiKey = "b03a640e5ef6980o4da35b006t5f2942";
       const url = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}`;
 
       try {
         const res = await axios.get(url);
-        setWeather({ data: res.data, loading: false, error: false }); // Set weather data
+        setWeather({ data: res.data, loading: false, error: false }); 
       } catch (error) {
-        setWeather({ data: {}, loading: false, error: true }); // Handle error
+        setWeather({ data: {}, loading: false, error: true }); 
         console.log("error", error);
       }
     }
   };
-
-  // Fetch default weather data for Rabat on component mount
   useEffect(() => {
     const fetchData = async () => {
       const apiKey = "b03a640e5ef6980o4da35b006t5f2942";
